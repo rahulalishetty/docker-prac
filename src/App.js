e840acd76744908fa0bd46a3b5ca42269e0c5a59
rahulalishetty <rahul.alishetty@zemosolabs.com>
2020-04-11 20:42:56 +0530

    asdfasd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello master jids
+          Hello master jidsads
         </p>
         <a
           className="App-link"
