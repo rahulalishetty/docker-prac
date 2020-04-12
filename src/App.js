882f8d66b60363cc1df588785fd4fce6960cc826
rahulalishetty <rahul.alishetty@zemosolabs.com>
2020-04-12 21:35:53 +0530

    Revert "asfddfsa"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello master ji| from feature branch
+          Hello master ji|
         </p>
         <a
           className="App-link"
